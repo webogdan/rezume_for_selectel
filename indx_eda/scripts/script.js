6713566132f582f39a3292f1c3287e414685f4fe
bagadun33 <85613196+webogdan@users.noreply.github.com>
2026-02-12 21:13:37 +0300

    Add files via upload

---
+++ indx_eda/scripts/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        items: 1,
+        loop: true,
+        nav: true,
+        dots: false,
+        autoplay: true,
+        autoplayHoverPause: true,
+        smartSpeed: 500
+    });
+    
+    $('#tabset').tabs();
+    
+});
